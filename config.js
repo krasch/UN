eff8fdd1df3736b4bebe2b161f4a1f1ac73bddab
krasch <krasch@kth.se>
2015-09-17 17:41:06 -0700

    Add axis labels, explanatory manual

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var goalColors = configureGoalColors(["#832a64", "#006800", "#2a6eb3", "#bb244d",
                                       "#878787", "#dc624d",  "#2c4c6c", "#8c510a"]);
 
-var circle = {width: 40, height: 40,
-              radius: 8, border: 3,
-              cx: 24, cy: 24}
+var circle = {width: 45, height: 45,
+              radius: 15, border: 3,
+              cx: 25, cy: 25}
 
-var bar = {margin : {top: 25, right: 80, bottom: 60, left: 80}}
+var bar = {margin : {top: 20, right: 15, bottom: 25, left: 70}}
     bar.width = 500 - bar.margin.left - bar.margin.right;
     bar.height= 200 - bar.margin.top - bar.margin.bottom;
 
